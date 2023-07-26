0ec103a372a7125d84fcb2c31365954c28452625
Daryll Ko <daryll.ko@gmail.com>
2023-07-26 11:14:14 +0800

    refactor: set n=1000 for now

--- src/lib/makeRow.tsx
+++ src/lib/makeRow.tsx
@@ -1,22 +1,27 @@
 import { type Device } from "~/components/table";
 
 function factorial(n: number): number {
-  return n === 0 ? 1 : n * factorial(n - 1);
+  let res = 1;
+  for (let i = 2; i <= n; ++i) {
+    res *= i;
+  }
+  return res;
 }
 
 export default function makeRow(deviceName: string, clockRate: number): Device {
-  const n = 10;
+  const n = 1000;
+  const p = 3;
 
   return {
     deviceName,
-    constant: 1 / clockRate,
-    log: Math.log2(n) / clockRate,
-    root: Math.sqrt(n) / clockRate,
-    linear: n / clockRate,
-    loglinear: (n * Math.log2(n)) / clockRate,
-    quadratic: (n * n) / clockRate,
-    cubic: (n * n * n) / clockRate,
-    exponential: Math.pow(2, n) / clockRate,
-    factorial: factorial(n) / clockRate,
+    constant: (1 / clockRate).toPrecision(p),
+    log: (Math.log2(n) / clockRate).toPrecision(p),
+    root: (Math.sqrt(n) / clockRate).toPrecision(p),
+    linear: (n / clockRate).toPrecision(p),
+    loglinear: ((n * Math.log2(n)) / clockRate).toPrecision(p),
+    quadratic: ((n * n) / clockRate).toPrecision(p),
+    cubic: ((n * n * n) / clockRate).toPrecision(p),
+    exponential: (Math.pow(2, n) / clockRate).toPrecision(p),
+    factorial: (factorial(n) / clockRate).toPrecision(p),
   };
 }
