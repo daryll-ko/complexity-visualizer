48422f5d59d6692ba65b35a4c71db0747faff70e
Daryll Ko <daryll.ko@gmail.com>
2023-08-02 12:26:48 +0800

    feat: make `n` editable

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -4,6 +4,7 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { useMemo } from "react";
 import { InlineMath } from "react-katex";
 import makeRow from "~/lib/makeRow";
 
@@ -20,61 +21,70 @@ export interface Device {
   factorial: string;
 }
 
-const data: Device[] = [
-  makeRow("Arduino Uno (16 MHz)", 16000000),
-  makeRow("Nintendo DS (67 MHz)", 67000000),
-  makeRow("Nintendo 3DS (268 MHz)", 268000000),
-  makeRow("Nintendo Switch (1.02 GHz)", 1020000000),
-  makeRow("Macbook Pro M1 (3.2 GHz)", 3200000000),
-  makeRow("PlayStation 5 (3.5 GHz)", 3500000000),
-];
+interface Props {
+  n: number;
+}
 
-const columnHelper = createColumnHelper<Device>();
+export default function Table({ n }: Props) {
+  const makeData = (n: number): Device[] => {
+    return [
+      makeRow("Arduino Uno (16 MHz)", 16000000, n),
+      makeRow("Nintendo DS (67 MHz)", 67000000, n),
+      makeRow("Nintendo 3DS (268 MHz)", 268000000, n),
+      makeRow("Apple Watch (520 MHz)", 520000000, n),
+      makeRow("Nintendo Switch (1.02 GHz)", 1020000000, n),
+      makeRow("Macbook Pro M1 (3.2 GHz)", 3200000000, n),
+      makeRow("PlayStation 5 (3.5 GHz)", 3500000000, n),
+    ];
+  };
 
-const columns = [
-  columnHelper.accessor("deviceName", {
-    cell: (info) => info.getValue(),
-    header: () => <span>Device Name</span>,
-  }),
-  columnHelper.accessor("constant", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(1)" />,
-  }),
-  columnHelper.accessor("log", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(\lg n)" />,
-  }),
-  columnHelper.accessor("root", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(\sqrt{n})" />,
-  }),
-  columnHelper.accessor("linear", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(n)" />,
-  }),
-  columnHelper.accessor("loglinear", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(n\lg n)" />,
-  }),
-  columnHelper.accessor("quadratic", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(n^{2})" />,
-  }),
-  columnHelper.accessor("cubic", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(n^{3})" />,
-  }),
-  columnHelper.accessor("exponential", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(2^{n})" />,
-  }),
-  columnHelper.accessor("factorial", {
-    cell: (info) => info.getValue(),
-    header: () => <InlineMath math="\Theta(n!)" />,
-  }),
-];
+  const data = useMemo(() => makeData(n), [n]);
 
-export default function Table() {
+  const columnHelper = createColumnHelper<Device>();
+
+  const columns = [
+    columnHelper.accessor("deviceName", {
+      cell: (info) => info.getValue(),
+      header: () => <span>Device Name</span>,
+    }),
+    columnHelper.accessor("constant", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(1)" />,
+    }),
+    columnHelper.accessor("log", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(\lg n)" />,
+    }),
+    columnHelper.accessor("root", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(\sqrt{n})" />,
+    }),
+    columnHelper.accessor("linear", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(n)" />,
+    }),
+    columnHelper.accessor("loglinear", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(n\lg n)" />,
+    }),
+    columnHelper.accessor("quadratic", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(n^{2})" />,
+    }),
+    columnHelper.accessor("cubic", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(n^{3})" />,
+    }),
+    columnHelper.accessor("exponential", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(2^{n})" />,
+    }),
+    columnHelper.accessor("factorial", {
+      cell: (info) => info.getValue(),
+      header: () => <InlineMath math="\Theta(n!)" />,
+    }),
+  ];
+
   const table = useReactTable({
     data,
     columns,
@@ -116,22 +126,6 @@ export default function Table() {
           </tr>
         ))}
       </tbody>
-      {/* <tfoot>
-        {table.getFooterGroups().map((footerGroup) => (
-          <tr key={footerGroup.id}>
-            {footerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.footer,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </tfoot> */}
     </table>
   );
 }
--- src/lib/makeRow.tsx
+++ src/lib/makeRow.tsx
@@ -1,6 +1,10 @@
 import { type Device } from "~/components/table";
 
 function factorial(n: number): number {
+  return 1;
+  if (n >= 100) {
+    return Infinity;
+  }
   let res = 1;
   for (let i = 2; i <= n; ++i) {
     res *= i;
@@ -8,8 +12,19 @@ function factorial(n: number): number {
   return res;
 }
 
-export default function makeRow(deviceName: string, clockRate: number): Device {
-  const n = 1000;
+function safePow(n: number): number {
+  return 1;
+  if (n >= 100) {
+    return Infinity;
+  }
+  return Math.pow(2, n);
+}
+
+export default function makeRow(
+  deviceName: string,
+  clockRate: number,
+  n: number
+): Device {
   const p = 3;
 
   return {
@@ -21,7 +36,7 @@ export default function makeRow(deviceName: string, clockRate: number): Device {
     loglinear: ((n * Math.log2(n)) / clockRate).toPrecision(p),
     quadratic: ((n * n) / clockRate).toPrecision(p),
     cubic: ((n * n * n) / clockRate).toPrecision(p),
-    exponential: (Math.pow(2, n) / clockRate).toPrecision(p),
+    exponential: (safePow(n) / clockRate).toPrecision(p),
     factorial: (factorial(n) / clockRate).toPrecision(p),
   };
 }
